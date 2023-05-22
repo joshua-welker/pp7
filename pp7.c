1be24ddaa36fcbddf30c04f51e3f75e5b82d6645
joshua-welker <joshua.welker@stud.thga.de>
2023-05-22 19:18:09 +0200

    Demonstration des Gebrauchs von Datentypen

---
+++ pp7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main (){
+	int integer=42;
+	char character='a';
+	float rationalNumber=1.21;
+	_Bool boolean=true;
+	printf("Ausgabe über die entsprechende Variable\n int: %i \n char: %c \n float: %f \n bool: %d \n", integer, character, rationalNumber, boolean);
+	return 0;
+}
